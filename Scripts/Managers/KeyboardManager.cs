4506a5e94c17b41f8531ab31282c54e27c2c33a9
skipgamer <tpilepich@gmail.com>
2015-09-18 00:43:22 +1000

    first real commit
    just added keyboard hooks for modifier keys

--- Scripts/Managers/KeyboardManager.cs
+++ Scripts/Managers/KeyboardManager.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
 using System.Collections;
 
-public class KeyboardManager : MonoBehaviour {
+public class KeyboardManager : MonoBehaviour
+{
 
     GameManager gameManager;
+
+    [HideInInspector]
+    public bool altModifier;
+    [HideInInspector]
+    public bool shiftModifier;
+    [HideInInspector]
+    public bool ctrlModifier;
 
     // Use this for initialization
     void Start ()
     {
         gameManager = this.GetComponent<GameManager>();
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-	    if (Input.GetButton("PanUp"))
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("PanUp"))
         {
             gameManager.cameraManager.MoveCamera(Vector3.forward);
         }
@@ -29,6 +39,13 @@ public class KeyboardManager : MonoBehaviour {
         {
             gameManager.cameraManager.MoveCamera(Vector3.right);
         }
+
+        if (Input.GetButtonDown("AltModifier")) altModifier = true;
+        if (Input.GetButtonUp("AltModifier")) altModifier = false;
+        if (Input.GetButtonDown("ControlModifier")) ctrlModifier = true;
+        if (Input.GetButtonUp("ControlModifier")) ctrlModifier = false;
+        if (Input.GetButtonDown("ShiftModifier")) shiftModifier = true;
+        if (Input.GetButtonUp("ShiftModifier")) shiftModifier = false;
 
     }
 }
